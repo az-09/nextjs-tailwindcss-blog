07367d3f21d2ddb459cb6e24f1efb573bec50605
Tae Hee Choi <dadac76@hotmail.com>
2021-10-13 23:05:04 -0700

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <div>
+                <article class="prose">
+                    <h1 >{postData.title}</h1>
+                    <div>
+                        <Date dateString={postData.date} />
+                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+
+                </article>
+            </div>
+
+
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
