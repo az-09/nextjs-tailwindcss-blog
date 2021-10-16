459cdb419fb606d369cae296fb1e3a04bd041c8e
Tae Hee Choi <dadac76@hotmail.com>
2021-10-15 23:36:30 -0700

    color schema

--- components/BlogList.js
+++ components/BlogList.js
@@ -7,7 +7,8 @@ export default function BlogList({ allPostsData }) {
             <div className="max-w-6xl mx-auto h-48">
                 <ul>
                     {allPostsData.map(({ id, date, title, description }) => (
-                        <li className="px-8 py-2 border-b border-card-border hover:text-blue-800" key={id}>
+                        <li className="text-xl text-gray-700 px-8 py-2 border-b hover:text-[]" key={id}>
+                            
                             <Link href={`/posts/${id}`}>
                                 <a>
                                     <div className="text-xl font-medium">{title}</div>
