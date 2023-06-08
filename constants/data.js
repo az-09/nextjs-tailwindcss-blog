7f6950e3d10e1b8a8ed88ad01d6bf842f8383c0d
Tae Hee Choi <dadac76@hotmail.com>
2023-06-07 23:10:42 -0700

    removed www

--- constants/data.js
+++ constants/data.js
@@ -1,76 +1,82 @@
 export const userData = {
-    githubUsername: "taeheechoi",
-    name: "taeheechoi",
-    designation: "Developer",
-    avatarUrl: "/avatar.png",
-    email: "programmer.az09@gmail.com",
-    projects: [
-        {
-            title: "ERP Searching",
-            link: "https://erpsearching.taeheechoi.com/",
-            imgUrl: "/erpsearch.png",
-            description: [`Search data from multiple ERP systems`,],
-            stack: "React, Redux, Elastic UI, JSON server, Faker",
-            github: "https://github.com/taeheechoi/nextjs-tailwindcss-blog",
-
-        },
-        {
-            title: "Blog",
-            link: "https://www.taeheechoi.com/",
-            imgUrl: "/blog.jpg",
-            description: [`My blog to share my learnings in life and programming. - 2021`,],
-            stack: "NextJs, Tailwindcss, Vercel",
-            github: "https://github.com/taeheechoi/nextjs-tailwindcss-blog",
-
-        },
-        {
-            title: "Lotto Combo",
-            link: "https://lottocombo.taeheechoi.com",
-            imgUrl: "/lottocombo.jpg",
-            description: [`Analyzing frequency of combinations in lottery winning numbers. - 2021`,],
-            stack: "React, Django, Postgres, Docker, AWS",
-            github: "https://github.com/taeheechoi/lotto-combo-deployment",
-
-        },
-        {
-            title: "ME Contacts",
-            link: "https://mecontacts.taeheechoi.com",
-            imgUrl: "/mecontacts.jpg",
-            description: [`Simple CRUD contact app with authentication and search. Tutorial from Cryce Truly. - 2021`,],
-            stack: "React, Django, Postgres, Docker, AWS",
-            github: "https://github.com/taeheechoi/mecontacts-django-react-iis",
-        },
-        {
-            title: "Stock Monitor",
-            link: "",
-            imgUrl: "/stockmonitor.jpg",
-            description: [`Notification email when app detects abnormal activities in stocks monitored. - 2020`,],
-            stack: "Python, GoogleAPI",
-            github: "",
-        },
-        {
-            title: "Lost In Investment",
-            link: "https://lostininvestment.taeheechoi.com",
-            imgUrl: "lostininvestment.jpg",
-            description: [`Analyzing a specific stock based on volume and price actions. - 2019`,],
-            stack: "Flask, Docker, AWS",
-            github: "https://github.com/taeheechoi/mecontacts-django-react-iis",
-        },
-      
-    ],
-    about: {
-        title:
-            "I am Taehee Choi passionate about developing applications and automation.",
-        description: [
-            `I joined the party of web programming world a little bit late after being around in ERP developments,
+  githubUsername: "taeheechoi",
+  name: "taeheechoi",
+  designation: "Developer",
+  avatarUrl: "/avatar.png",
+  email: "programmer.az09@gmail.com",
+  projects: [
+    {
+      title: "ERP Searching",
+      link: "https://erpsearching.taeheechoi.com/",
+      imgUrl: "/erpsearch.png",
+      description: [`Search data from multiple ERP systems`],
+      stack: "React, Redux, Elastic UI, JSON server, Faker",
+      github: "https://github.com/taeheechoi/nextjs-tailwindcss-blog",
+    },
+    {
+      title: "Blog",
+      link: "https://taeheechoi.com/",
+      imgUrl: "/blog.jpg",
+      description: [
+        `My blog to share my learnings in life and programming. - 2021`,
+      ],
+      stack: "NextJs, Tailwindcss, Vercel",
+      github: "https://github.com/taeheechoi/nextjs-tailwindcss-blog",
+    },
+    {
+      title: "Lotto Combo",
+      link: "https://lottocombo.taeheechoi.com",
+      imgUrl: "/lottocombo.jpg",
+      description: [
+        `Analyzing frequency of combinations in lottery winning numbers. - 2021`,
+      ],
+      stack: "React, Django, Postgres, Docker, AWS",
+      github: "https://github.com/taeheechoi/lotto-combo-deployment",
+    },
+    {
+      title: "ME Contacts",
+      link: "https://mecontacts.taeheechoi.com",
+      imgUrl: "/mecontacts.jpg",
+      description: [
+        `Simple CRUD contact app with authentication and search. Tutorial from Cryce Truly. - 2021`,
+      ],
+      stack: "React, Django, Postgres, Docker, AWS",
+      github: "https://github.com/taeheechoi/mecontacts-django-react-iis",
+    },
+    {
+      title: "Stock Monitor",
+      link: "",
+      imgUrl: "/stockmonitor.jpg",
+      description: [
+        `Notification email when app detects abnormal activities in stocks monitored. - 2020`,
+      ],
+      stack: "Python, GoogleAPI",
+      github: "",
+    },
+    {
+      title: "Lost In Investment",
+      link: "https://lostininvestment.taeheechoi.com",
+      imgUrl: "lostininvestment.jpg",
+      description: [
+        `Analyzing a specific stock based on volume and price actions. - 2019`,
+      ],
+      stack: "Flask, Docker, AWS",
+      github: "https://github.com/taeheechoi/mecontacts-django-react-iis",
+    },
+  ],
+  about: {
+    title:
+      "I am Taehee Choi passionate about developing applications and automation.",
+    description: [
+      `I joined the party of web programming world a little bit late after being around in ERP developments,
             but I have a big hope that my passion in learning programming will bring me luck and open a door to a world where I can communicate with you.`,
-            `I will share my learnings as much as possible, as easy as possible and as concise as possible with you, so stay tuned.`,
-            `-- Start simple and add a block one at a time -- by me`,
-        ],
-        imgUrl: "/profile.jpg",
-    },   
-    socialLinks: {
-        linkedin: "https://www.linkedin.com/in/tae-hee-c-68b130172/",
-        github: "https://github.com/taeheechoi",
-    }
+      `I will share my learnings as much as possible, as easy as possible and as concise as possible with you, so stay tuned.`,
+      `-- Start simple and add a block one at a time -- by me`,
+    ],
+    imgUrl: "/profile.jpg",
+  },
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/tae-hee-c-68b130172/",
+    github: "https://github.com/taeheechoi",
+  },
 };
